a4c9c2215cbe00d0e2b4d33e05d140924e014db4
Anish <anishkakaiya@outlook.com>
2022-06-19 21:51:37 +0100

    show notes on webpage with div dynamically

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 console.log('App is running');
 
 const notesModel = require('./notesModel');
+const notesView = require('./notesView');
 
 const notes = new notesModel
+const view = new notesView(notes)
+
 console.log(notes.getNotes())
 
+notes.addNote('This is an example note');
+view.displayNotes();
