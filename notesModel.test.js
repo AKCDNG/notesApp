a0a6a7c7f6681ef18c08ffc06fff8b0638738b77
Anish <anishkakaiya@outlook.com>
2022-06-19 20:04:14 +0100

    add test to add note

--- notesModel.test.js
+++ notesModel.test.js
@@ -5,4 +5,10 @@ describe("Notes", () => {
     const notes = new notesModel;
     expect(notes.getNotes()).toEqual([]);
   })
+
+  it('Adds a note when addNote is called', () => {
+    const notes = new notesModel;
+    notes.addNote('Buy milk');
+    expect(notes.getNotes()).toEqual(['Buy milk'])
+  })
 })
