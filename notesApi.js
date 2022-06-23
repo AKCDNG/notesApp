1228f2bf376ad1256daea8ad8ad9bea3ebc70822
Anish <anishkakaiya@outlook.com>
2022-06-23 14:22:01 +0100

    change from using promises to using async await

--- notesApi.js
+++ notesApi.js
@@ -1,11 +1,12 @@
 class notesApi {
-  loadNotes () {
-    fetch('http://localhost:3000/notes')
-    .then((response) => response.json()) // 1. convert JSON to JS object
-    .then((data) => {
-      // 2. `data` is now a full JS object, so we can access its properties  
-      return(data)
-    });
+  async loadNotes() {
+    try {
+      const response = await fetch("http://localhost:3000/notes");
+      const notes = await response.json();
+      return notes;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
