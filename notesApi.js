de19fc61dc638c813565480eb7883232141340f8
Anish <anishkakaiya@outlook.com>
2022-06-23 12:14:50 +0100

    add code to notes api to call back from backend

--- notesApi.js
+++ notesApi.js
@@ -1,10 +1,10 @@
 class notesApi {
-  loadNotes (callback) {
+  loadNotes () {
     fetch('http://localhost:3000/notes')
     .then((response) => response.json()) // 1. convert JSON to JS object
     .then((data) => {
       // 2. `data` is now a full JS object, so we can access its properties  
-      callback(data)
+      return(data)
     });
   }
 }
