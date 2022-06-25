1a00feca99a3293cef84cda063427636f5da7f74
Anish <anishkakaiya@outlook.com>
2022-06-25 16:49:17 +0100

    add post method to API

--- notesApi.js
+++ notesApi.js
@@ -8,6 +8,22 @@ class notesApi {
       console.log(error);
     }
   }
+
+  async createNote(note) {
+    try {
+      const response = await fetch("http://localhost:3000/notes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content: note }),
+      });
+      const notes = await response.json();
+      return notes;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 module.exports = notesApi;
